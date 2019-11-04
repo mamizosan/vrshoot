c329337cc91aa5732ee1e1d7bf988d6257627ce0
mamizosan <51304040+mamizosan@users.noreply.github.com>
2019-11-05 08:09:59 +0900

    commit
    commit

---
+++ Assets/Dummy/UITest.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UITest : MonoBehaviour {
+	RectTransform _rect = null;
+	// Use this for initialization
+	void Start () {
+		_rect = GetComponent< RectTransform >( );
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		_rect.transform.position += new Vector3( 1, 0, 0 );
+	}
+}
